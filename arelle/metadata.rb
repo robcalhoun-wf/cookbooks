e2e8f102764232ec5d43e8a878551b49c95753bf
Rob Calhoun <rob.calhoun@webfiliings.com>
2012-04-05 15:30:19 -0600

    Made arelle cookbook work

--- arelle/metadata.rb
+++ arelle/metadata.rb
@@ -5,6 +5,8 @@ description      "Installs/Configures arelle"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "python"
+
 recipe "arelle::default","Installed arelle"
 
 
